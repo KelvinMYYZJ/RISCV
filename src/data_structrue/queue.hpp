3b7eb71bbda646debc5dfdfab22933956bf2affe
KelvinMYYZJ <Kelvin_MYYZJ@outlook.com>
2022-06-30 10:29:25 +0800

    opt

--- src/data_structrue/queue.hpp
+++ src/data_structrue/queue.hpp
@@ -23,7 +23,7 @@ struct Queue {
   }
   bool Full() { return tail + 1 == head || tail + 1 == head + 32; }
   bool Empty() { return head == tail; }
-  T& operator[](int idx) { return val[idx]; }
+  T& operator[](const int& idx) { return val[idx]; }
   T& Front() { return val[head]; }
   void Clear() {
     head = 0;
